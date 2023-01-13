4c52eed8659758bb71abf1c56cf93626c2b6c926
dxtobi <akanbijosephtobi@gmail.com>
2023-01-13 13:47:31 -0800

    fixing build problem

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+
 };
 
 export default config;
