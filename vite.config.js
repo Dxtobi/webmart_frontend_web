5a64f8f989ee3dff46bdeed40515ca06e001ffcd
dxtobi <akanbijosephtobi@gmail.com>
2023-01-13 22:16:50 -0800

    trying svelte

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
 	plugins: [sveltekit()],
-
+	optimizeDeps: {
+		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+	  }
 };
 
 export default config;
